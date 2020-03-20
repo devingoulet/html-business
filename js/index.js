be8c34532b0e1d086675e1ff27632bd726aed238
Devin <devingoulet@icloud.com>
2020-03-20 13:08:12 -0700

    ajustments

--- js/index.js
+++ js/index.js
@@ -22,14 +22,36 @@ console.log(question);
 var topHat;
 
 if (question === 'yes') { // ===  strictly equal to     !== not strictly equal to  true or false
-    topHat = 'Great, continue that path with us.';
+    topHat = confirm('Great, continue that path with us.');
 } else if (question === 'no'){
-    topHat = 'Do you want to be?'
+    topHat = prompt('Do you want to be?');
 } else {
-    topHat = 'Try again please'
+    topHat = prompt('Try again please');
 }
 
-document.write('<h4>' + topHat + '</h4>');
-for (var i = 0; i < 5; i++) {
-    document.write(i)
+//document.write('<h4>' + topHat + '</h4>');
+
+function howManyBottles() {
+    var bottles = prompt("How many bottles do you want to purchase?");
+    //var numBottles = 0;
+    console.log(typeof(bottles));
+   
+    while (bottles === '0' || bottles > 5){
+        confirm('hey you do not have the right number!');
+        bottles = prompt("Number of bottles you want. (Answer with a number between 0 and 5.)");
+    }
+
+    //we have the number of bottles purchased by the user in the variable bottles
+    // we weant to create a for loop for the number of bottles
+    // we want to generate the image of the bottle for the number of times chosen by the bottles
+    for (var counter = 0; counter < bottles; counter++) {
+       
+         document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Bouteille.jpg/800px-Bouteille.jpg" width: "100" height: "100">');
+    }
+
+
 }
+
+
+
+howManyBottles();
